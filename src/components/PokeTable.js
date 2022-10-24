3c81d688898e5d37908bb942dc7f65bd044d724f
Kevin <kmadden84@gmail.com>
2022-10-24 18:32:06 -0400

    Update PokeTable.js

--- src/components/PokeTable.js
+++ src/components/PokeTable.js
@@ -1,5 +1,12 @@
 export default function PokeTable({ pokemonData = [] }) {
 
+  function formatList(pokemonTypes, type, index) {
+    if (index !== pokemonTypes.length - 1 && pokemonTypes.length > 0) {
+      return `${type.type.name}, `
+    } else {
+      return type.type.name
+    }
+  }
 
   return pokemonData && (
     <div>
@@ -8,15 +15,19 @@ export default function PokeTable({ pokemonData = [] }) {
           <tr>
             <th>IMAGE</th>
             <th>NAME</th>
-            <th>Types</th>
+            <th>TYPES</th>
           </tr>
           {
             pokemonData?.map((pokemon, index) => (
               <tr key={index}>
-                <td><img src={pokemon?.image_url} alt="pokemon image" /></td>
+                <td><img src={pokemon?.image_url} /></td>
                 <td>{pokemon?.name?.toString()}</td>
-                <td>TYPE(S):{pokemon?.types?.map((type, index) => (
-                  <span key={index} > {type.type.name}</span>
+                <td>{pokemon?.types?.map((type, index) => (
+                  <span key={index}>
+
+                    {formatList(pokemon?.types, type, index)}
+
+                  </span>
                 ))}
                 </td>
               </tr>
