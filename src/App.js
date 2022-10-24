b72ce21036267dd1457bce8fc04e9e45e7874b6c
Kevin <kmadden84@gmail.com>
2022-10-24 18:12:18 -0400

    Pokemon Table

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useEffect, useState } from 'react';
+import PokeTable from './components/PokeTable';
+
+function App() {
+
+  const [pokemon, setPokemon] = useState(null);
+  const [pokemonInfo, setPokemonInfo] = useState([])
+
+  const fetchPokemon = async () => {
+    const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+    const data = await pokemons.json();
+    setPokemon(data);
+  }
+
+  const fetchPokemonDetails = async () => {
+    const allPokemons = pokemon.results;
+    allPokemons?.forEach(async function (item) {
+
+      const pokemonUrl = item.url;
+      const pokemonData = await fetch(pokemonUrl);
+      const data = await pokemonData.json();
+
+      const formattedData = {
+        name: data?.name,
+        types: data?.types,
+        image_url: data?.sprites?.front_default
+      };
+      setPokemonInfo(prev => prev.concat(formattedData))
+    })
+  }
+
+  useEffect(function retrieveList() {
+    if (!pokemon) fetchPokemon();
+  }, [pokemon])
+
+
+  useEffect(function retrieveDetails() {
+    if (pokemon && !pokemonInfo.length) fetchPokemonDetails();
+  }, [pokemonInfo, pokemon])
+
+
+  return (
+    <div className="App">
+      {pokemonInfo?.length > 0 ? <PokeTable pokemonData={pokemonInfo} /> : null}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PokeTable.js
@@ -0,0 +1,32 @@
+import Reac, { useEffect } from 'react'
+
+
+export default function PokeTable({ pokemonData = [] }) {
+
+
+  return pokemonData && (
+    <div>
+      <table className="pokeTable">
+        <tbody border="1" align="center">
+          <tr>
+            <th>IMAGE</th>
+            <th>NAME</th>
+            <th>Types</th>
+          </tr>
+          {
+            pokemonData?.map((pokemon, index) => (
+              <tr key={index}>
+                <td><img src={pokemon?.image_url} /></td>
+                <td>{pokemon?.name?.toString()}</td>
+                <td>TYPE(S):{pokemon?.types?.map((type, index) => (
+                  <span> {type.type.name}</span>
+                ))}
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
